d414f1cc4494b92bcffbd42014198055605cf158
Darren Wu <darren.wu@canonical.com>
2017-08-28 17:24:10 +0800

    Add script to test iio/i2c sysfs

---
+++ iio/iio_sysfs_test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "read from iio sysfs"
+cat /sys/bus/iio/devices/iio:device1/scan_elements/in_timestamp_en
+echo "read from i2c sysfs"
+cat /sys/devices/platform/80860F41:00/i2c-0/i2c-SMO9210:00/iio:device2/scan_elements/in_temp_en
+echo "write to iio sysfs"
+echo 1 > /sys/bus/iio/devices/iio:device1/scan_elements/in_timestamp_en
+echo "write to i2c sysfs"
+echo 1 > /sys/devices/platform/80860F41:00/i2c-0/i2c-SMO9210:00/iio:device2/scan_elements/in_temp_en
