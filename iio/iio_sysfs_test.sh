52941d84e4ed5664ae649879afe71031391f2258
Darren Wu <darren.wu@canonical.com>
2017-08-31 11:07:15 +0800

    dynamic detect iio device number which include temperature

--- iio/iio_sysfs_test.sh
+++ iio/iio_sysfs_test.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "read from iio sysfs"
-cat /sys/bus/iio/devices/iio:device1/scan_elements/in_temp_en
-echo "read from i2c sysfs"
-cat /sys/devices/platform/80860F41:00/i2c-0/i2c-SMO9210:00/iio:device1/scan_elements/in_temp_en
-echo "write to iio sysfs"
-echo 1 > /sys/bus/iio/devices/iio:device1/scan_elements/in_temp_en
-echo "write to i2c sysfs"
-echo 1 > /sys/devices/platform/80860F41:00/i2c-0/i2c-SMO9210:00/iio:device1/scan_elements/in_temp_en
+for device in device1 device2 device3
+do
+    if [ -f /sys/bus/iio/devices/iio:$device/scan_elements/in_temp_en ];then
+        echo "read from iio sysfs"
+        cat /sys/bus/iio/devices/iio:$device/scan_elements/in_temp_en
+        echo "read from i2c sysfs"
+        cat /sys/devices/platform/80860F41:00/i2c-0/i2c-SMO9210:00/iio:$device/scan_elements/in_temp_en
+        echo "write to iio sysfs"
+        echo 1 > /sys/bus/iio/devices/iio:$device/scan_elements/in_temp_en
+        echo "write to i2c sysfs"
+        echo 1 > /sys/devices/platform/80860F41:00/i2c-0/i2c-SMO9210:00/iio:$device/scan_elements/in_temp_en
+        break
+    fi
+done
